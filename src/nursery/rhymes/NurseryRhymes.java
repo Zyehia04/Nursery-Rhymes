c366c4cb89548d121076affc4e927be260cb4c5f
21399 <21399@MININT-MPKMA8T.columbushs.com>
2019-09-19 13:50:02 -0400

    Done

--- src/nursery/rhymes/NurseryRhymes.java
+++ src/nursery/rhymes/NurseryRhymes.java
@@ -15,33 +15,56 @@ public class NurseryRhymes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         farm("cow", "moo");
-        farm("duck", "quack");        
+        farm("duck", "quack");
+        farm("Angry Bird", "Squaa");
         // TODO: add another animal to the farm here
+        
+        monkeys(10);
+        monkeys(9);
+        
+        
         hickory_dickory(1);
         hickory_dickory(2);
+        hickory_dickory(3);
+        
         // TODO: make the clock strike three here
-        
-        // TODO: call your new methods here ( you must write them first! ) 
-        
-        
+        bruhmode("Medina");
+        bruhmode("Angry");
+              
     }
-    
-    
-    
-    public static void farm(String animal, String sound){
+
+    public static void farm(String animal, String sound) {
         // TODO: write your code here
+        System.out.println("Old MacDonald had a farm\n"
+                + "e-i-e-i-o\n"
+                + "And on that farm he had a " + animal + "\n"
+                + "e-i-e-i-o\n"
+                + "With a " + sound + " " + sound + " here\n"
+                + "");
     }
-    
-    public static void monkeys(int number){
+
+    public static void monkeys(int number) {
         // TODO: write your code here
+        System.out.println(number + " ​little​ ​monkeys​ ​jumping​ ​on​ ​the​ ​bed\n"
+                + "One​ ​fell​ ​off​ ​and​ ​bumped​ ​his​ ​head\n"
+                + "Mama​ ​called​ ​the​ ​doctor,​ ​and​ ​the​ ​doctor​ ​said\n"
+        + "\"No​ ​more​ ​monkeys​ ​jumping​ ​on​ ​the​ ​bed!\"\n");
     }
-    
-    public static void hickory_dickory(int time){
+
+    public static void hickory_dickory(int time) {
         // TODO: write your code here
+        System.out.println("\nHickory​ ​dickory​ ​dock\n"
+                + "The​ ​mouse​ ​ran​ ​up​ ​the​ ​clock\n"
+                + "The​ ​clock​ ​struck​ " + time + "\n"
+                + "The​ ​mouse​ ​ran​ ​down\n"
+                + "Hickory​ ​dickory​ ​dock ");
     }
-    
+
     // TODO add your new methods here
-    
-}
+    public static void bruhmode(String person){
+        System.out.println("\n" + person + " cant do a pullup\n"
+                + "ay yuh\n");
+    }
+}   
